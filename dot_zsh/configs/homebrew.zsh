a474ee9120e52118a88293bdf1e9a8ccbc808f93
Michael Lee <michael@mrlee.io>
2022-11-18 19:32:22 +0000

    New dotfiles configuration for Warp terminal

---
+++ dot_zsh/configs/homebrew.zsh
@@ -0,0 +1,5 @@
+# To opt in to Homebrew analytics, `unset` this in ~/.zshrc.local .
+# Learn more about what you are opting in to at
+# https://docs.brew.sh/Analytics
+
+export HOMEBREW_NO_ANALYTICS=1
