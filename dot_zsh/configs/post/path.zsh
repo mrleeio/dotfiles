5587f1ec8b066bf856942bfc3f392d710f8bb775
Michael Lee <michael@mrlee.io>
2022-11-18 14:08:21 -0700

    Add path configuration for chruby & fnm

--- dot_zsh/configs/post/path.zsh
+++ dot_zsh/configs/post/path.zsh
@@ -4,4 +4,11 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+# Chruby - https://github.com/postmodern/chruby
+source $HOMEBREW_PREFIX/opt/chruby/share/chruby/chruby.sh
+source $HOMEBREW_PREFIX/opt/chruby/share/chruby/auto.sh
+
+# fnm - https://github.com/Schniz/fnm
+eval "$(fnm env --use-on-cd)"
+
 export -U PATH
