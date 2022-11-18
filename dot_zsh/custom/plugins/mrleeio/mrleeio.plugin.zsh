c88c515212d7d54423eb17d625a1125bcb09b734
Michael Lee <michael@mrlee.io>
2022-11-18 09:58:15 -0700

    Update mrleeio.plugin.zsh
    Remove aliases that are no longer in use.

--- dot_zsh/custom/plugins/mrleeio/mrleeio.plugin.zsh
+++ dot_zsh/custom/plugins/mrleeio/mrleeio.plugin.zsh
@@ -25,17 +25,10 @@ export PATH=$PATH:$(go env GOPATH)/bin
 
 alias dev="cd $HOME/GitHub"
 alias mdev="cd $HOME/GitHub/mrleeio"
-alias vdev="cd $HOME/GitHub/verygoodsecurity"
 
 alias vimup="vim -u $HOME/.vimrc.bundles +PlugUpdate +PlugClean! +qa"
 
 alias ch="chezmoi"
-alias cha="chezmoi init --apply git@github.com:mrleeio/dotfiles.git"
-
-alias thi="pip3 install things-cli"
-alias th="things-cli"
-alias thva="things-cli -a 5DmEHnWdVDnAbGKRzNpuGd --recursive anytime"
-alias tht="things-cli today"
 
 alias be="bundle exec"
 alias ber="bundle exec rubocop"
