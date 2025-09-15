5eab83e92c8c8f375e6c0b766fa5856f368b4add
Michael Lee <michael@mrlee.io>
2025-09-15 16:08:49 -0700

    2025-09 updates

--- dot_zsh/configs/post/path.zsh
+++ dot_zsh/configs/post/path.zsh
@@ -4,17 +4,7 @@ PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
-# Chruby - https://github.com/postmodern/chruby
-source $HOMEBREW_PREFIX/opt/chruby/share/chruby/chruby.sh
-source $HOMEBREW_PREFIX/opt/chruby/share/chruby/auto.sh
-
-# Ruby Install src directory
-export RUBY_INSTALL_SRC_DIR="$HOME/.rubies/src"
-
-# fnm - https://github.com/Schniz/fnm
-eval "$(fnm env --use-on-cd)"
-
-# PostgreSQL 16
-PATH="/opt/homebrew/opt/postgresql@16/bin:$PATH"
+# mise - https://mise.jdx.dev/
+echo 'eval "$(mise activate zsh)"' >> ~/.zshrc
 
 export -U PATH
